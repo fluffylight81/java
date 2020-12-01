4d9751c7fc1382b3433d9396e6e09c016fde89cc
fluffylight81 <65219615+fluffylight81@users.noreply.github.com>
2020-12-01 18:07:15 +0300

    Создан проект для IDEA

---
+++ sandbox/build/docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"MyFirstProgram","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MyFirstProgram","l":"MyFirstProgram()","u":"%3Cinit%3E()"}];updateSearchResults();
